a106e333ba23f61ab26771255d0d2bc9360a2092
CarloMart88 <carlo.martino1988@gmail.com>
2025-08-29 16:19:41 +0200

    feat: Milestone1 ok

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,5 +1,44 @@
+const articles = [
+  {
+    id: 1,
+    title: "5 modi per rendere la pasta di casa irresistibile",
+  },
+  {
+    id: 2,
+    title: "Il risotto perfetto: segreti per una cremosità da chef",
+  },
+  {
+    id: 3,
+    title: "Erbe aromatiche: il tocco segreto per ogni piatto.",
+  },
+  {
+    id: 4,
+    title: "Pane come al forno: dalla lievitazione lenta alla crosta dorata.",
+  },
+  {
+    id: 5,
+    title: "L’olio extravergine: l’ingrediente che fa la differenza.",
+  },
+];
+
 function Main() {
-  return <h1>eccolo</h1>;
+  return (
+    <div className="col-12 m-2 ">
+      <div className="row">
+        <h2 className="text-center">I miei articoli :</h2>
+      </div>
+      <div className="row">
+        {articles.map((article) => {
+          const { id, title } = article;
+          return (
+            <div className="col-12 m-2 " key={id}>
+              {title}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Main;
